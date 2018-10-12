9e1ee807749b815ac5218c1db6f42f4488a64b0e
YuTin <yuting1987@gmail.com>
2018-10-12 17:45:47 +0800

    feat(package): added fragmentTypes

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
+import fs from 'fs';
+import { execFile } from './fragmentTypes';
+import jwt from './JsonWebToken';
 
-const JWT = require('./index');
-
-console.log(JWT.generateKey()); // eslint-disable-line
+switch (process.argv[1]) {
+  case 'key':
+    console.log(jwt.generateKey()); // eslint-disable-line
+    process.exit();
+    break;
+  case 'fragment':
+    execFile(require(process.argv[2])) // eslint-disable-line
+      .then(data => fs.writeFileSync(process.argv[3], data))
+      .then(() => process.exit());
+    break;
+  default:
+}
